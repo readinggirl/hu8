afdf861a52d9691d20c0e99443c204c5b12f0f82
Teresa <teresa.edinger@gmail.com>
2020-11-28 17:09:26 +0100

    project created and classes implemented

---
+++ src/hausuebung8/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hausuebung8;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            Executor runner = new Executor();
+
+            runner.runExample();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/hausuebung8/Storage.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hausuebung8;
+
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class Storage {
+
+    private final ArrayBlockingQueue<Integer> queue;
+
+    private int fetchedCounter;
+    private int storedCounter;
+    private int underflowCounter;
+    private int overflowCounter;
+    private boolean productionComplete;
+
+    public Storage() {
+        // implement this
+    }
+
+    public synchronized boolean put(Integer data) throws InterruptedException {
+        // implement this
+        return false;
+    }
+
+    public synchronized Integer get() {
+        // implement this
+        return null;
+    }
+
+    public boolean isProductionComplete() {
+        // implement this
+        return false;
+    }
+
+    public void setProductionComplete() {
+        // implement this
+    }
+
+    public int getFetchedCounter() {
+        // implement this
+        return -1;
+    }
+
+    public int getStoredCounter() {
+        // implement this
+        return -1;
+    }
+
+    public int getUnderflowCounter() {
+        // implement this
+        return -1;
+    }
+
+    public int getOverflowCounter() {
+        // implement this
+        return -1;
+    }
+}
